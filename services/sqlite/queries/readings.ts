69d53d06bc284d3d8a7055c97b4b7bd3b91b7656
Franco Speziali <developer@wemakemachines.com>
2025-04-05 19:17:47 +0200

    Replace better-sqlite3 with bun:sqlite

--- services/sqlite/queries/readings.ts
+++ services/sqlite/queries/readings.ts
@@ -1,4 +1,4 @@
-import type { Database } from "better-sqlite3";
+import type { Database } from "bun:sqlite";
 import { Reading } from "../../../types/types";
 
 export function selectReadings(db: Database) {
--- services/sqlite/schema/beacons.ts
+++ services/sqlite/schema/beacons.ts
@@ -1,4 +1,4 @@
-import type { Database } from "better-sqlite3";
+import type { Database } from "bun:sqlite";
 
 export function createTableBeacons(db: Database) {
   db.exec(`
