234ec7d724db27b87002ceec6af366b43d1fb41b
Franco Speziali <developer@wemakemachines.com>
2025-04-18 17:02:20 +0200

    Update readings page so user can toggle beacon data on graph

--- admin-panel/pages/readings/Readings.vue
+++ admin-panel/pages/readings/Readings.vue
@@ -1,13 +1,18 @@
 <template>
   <div v-if="readings">
     <h2>Chart</h2>
+    <ul>
+      <li v-for="(beacon, index) in beacons" :key="beacon.id">
+        <label>Beacon ID: {{ beacon.id }} <input type="checkbox" v-model="beacons[index].checked" /></label>
+      </li>
+    </ul>
     <div id="chart" style="width: 100%; height: 400px"></div>
   </div>
   <div v-else><p>No readings collected</p></div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 import { createLineChart, ChartDataInSeries } from "../../utils/chartist";
 
@@ -17,31 +22,55 @@ import "chartist/dist/index.css";
 
 const props = defineProps<{ initialReadings: ReadingsByBeaconId | null }>();
 const readings = ref(props.initialReadings);
+const beacons = ref(mapBeacons(readings.value));
+const lineChartData = ref(mapLuxToLineChart(readings.value));
 
-function mapLuxToLineChart(readings: ReadingsByBeaconId): { series: ChartDataInSeries[] } {
+watch(
+  () => beacons.value.map((b) => b.checked),
+  () => {
+    lineChartData.value = mapLuxToLineChart(readings.value);
+    drawChart();
+  },
+);
+
+function mapBeacons(readings: ReadingsByBeaconId | null) {
+  if (!readings) return [];
+
+  return Object.keys(readings).map((beaconId) => {
+    return { id: beaconId, checked: true };
+  });
+}
+
+function mapLuxToLineChart(readings: ReadingsByBeaconId | null): { series: ChartDataInSeries[] } | null {
+  if (!readings) return null;
+
   const series: ChartDataInSeries[] = [];
 
   for (const [beaconId, beaconData] of Object.entries(readings)) {
-    const luxData = beaconData.map((reading) => {
-      return { x: reading.timestamp, y: reading.lux };
-    });
+    const beacon = beacons.value.find((beacon) => beacon.id === beaconId);
 
-    series.push({ name: beaconId.toString(), data: luxData });
+    if (beacon?.checked) {
+      const luxData = beaconData.map((reading) => {
+        return { x: reading.timestamp, y: reading.lux };
+      });
+
+      series.push({ name: beaconId.toString(), data: luxData });
+    }
   }
-
-  console.log(series);
 
   return {
     series,
   };
 }
 
-onMounted(async () => {
-  if (readings.value) {
-    const lineChartData = mapLuxToLineChart(readings.value);
-
-    createLineChart("#chart", lineChartData);
+function drawChart() {
+  if (lineChartData.value) {
+    createLineChart("#chart", lineChartData.value);
   }
+}
+
+onMounted(async () => {
+  drawChart();
 });
 </script>
 
