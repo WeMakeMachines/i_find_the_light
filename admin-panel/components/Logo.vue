21da76eb4832171d09263a347581ab86ec841530
Franco Speziali <developer@wemakemachines.com>
2026-05-26 18:18:12 +0200

    Updated logo

--- admin-panel/components/Logo.vue
+++ admin-panel/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="margin-top: 20px; margin-bottom: 10px">
     <a href="/">
-      <img src="../assets/logo.svg" height="64" width="64" />
+      <img src="../assets/iftl-logo_small.png" height="64" width="64" />
     </a>
   </div>
 </template>
