81b502975530e5f9d0388f1899b81d32ebc8b05a
Franco Speziali <developer@wemakemachines.com>
2026-06-13 18:23:10 +0200

    Removed utils/ folder

--- utils/date.ts
+++
@@ -1,7 +0,0 @@
-export function timestampToMs(timestamp: number): number {
-  return timestamp * 1000;
-}
-
-export function timestampToS(timestamp: number): number {
-  return Math.round(timestamp / 1000);
-}
