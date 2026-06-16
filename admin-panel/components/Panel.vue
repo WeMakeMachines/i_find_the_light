f76adb29440f206ead36632467522a16c94e6306
Franco Speziali <developer@wemakemachines.com>
2026-06-16 23:21:38 +0200

    Add Panel component

---
+++ admin-panel/components/Panel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
+    <div class="bg-gray-700 p-3">
+      <slot name="header" />
+    </div>
+    <div class="p-3">
+      <slot />
+    </div>
+  </div>
+</template>
