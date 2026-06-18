7ef03239253d591b8b6347f406661369dc647c4e
Franco Speziali <developer@wemakemachines.com>
2026-06-18 13:49:16 +0200

    Update survey-detail page with beacon list and links to beacon detail pages

--- admin-panel/pages/survey-detail/@id/+Page.vue
+++ admin-panel/pages/survey-detail/@id/+Page.vue
@@ -70,7 +70,7 @@
         >
           <Link
             :disabled="Boolean(!readingsCount)"
-            href="#"
+            :href="'../readings/' + survey.id"
             class="flex text-white bg-green-500 inline-flex items-center hover:text-white hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center"
           >
             <svg
@@ -131,9 +131,7 @@
         <div v-if="survey.status === SurveyStatus.DRAFT" class="text-right">
           <Panel class="text-sm w-60 text-center">
             <template #header><p class="text-center text-white">Warning!</p></template>
-            <p class="text-left mb-5">
-              Activating this survey will de-activate any existing surveys, and prevent it from being edited.
-            </p>
+            <p class="text-left mb-5">Activating this survey will archive any existing active survey.</p>
             <button
               @click="activateSurvey"
               class="flex text-white bg-red-500 inline-flex items-center hover:text-white hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center"
@@ -210,11 +208,36 @@
       </template>
       <div v-if="beacons.length">
         <template v-for="beacon in beacons" :key="beacon.beaconId">
-          <div>
-            <p>ID: {{ beacon.beaconId }}</p>
-            <p>Name: {{ beacon.beaconName }}</p>
-            <p>Device Key: {{ beacon.deviceKey }}</p>
-          </div>
+          <Link
+            :href="`../beacon/${beacon.beaconId}/${beacon.surveyId}`"
+            class="flex text-xs px-1.5 py-0.5 rounded mb-4 bg-gradient-to-r from-teal-200 to-lime-200"
+          >
+            <svg
+              class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-miterlimit="10"
+                stroke-width="2"
+                d="M7.42584 18.2443c1.3169-2.2023 2.5334-1.5242 3.85026-3.7265 1.3169-2.2023.1004-2.8805 1.4173-5.08281 1.3169-2.20233 2.5335-1.52419 3.8504-3.72652M10.8472 20.1517c1.3169-2.2024 2.5334-1.5242 3.8503-3.7266 1.3169-2.2023.1004-2.8804 1.4173-5.0828 1.3169-2.20228 2.5334-1.52414 3.8503-3.72647l-6.8428-3.81455C11.8054 6.00361 10.5889 5.32547 9.272 7.5278s-.1004 2.8805-1.4173 5.0828c-1.3169 2.2023-2.5334 1.5242-3.85031 3.7265l6.84281 3.8146Z"
+              />
+            </svg>
+
+            <span class="mr-2"
+              >ID: <span class="text-base font-bold">{{ beacon.beaconId }}</span></span
+            >
+            <span class="mr-2"
+              >Name: <span class="text-base font-bold">{{ beacon.beaconName }}</span></span
+            >
+          </Link>
         </template>
       </div>
       <div v-else>No beacons registered</div>
