914fbb6a62d9bcb24ef3ca8bcd6db48f407bdc86
Franco Speziali <developer@wemakemachines.com>
2026-06-19 02:17:52 +0200

    Update sidebar

--- admin-panel/components/Logo.vue
+++ admin-panel/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="margin-top: 20px; margin-bottom: 10px">
-    <img src="../assets/iftl-logo.png" height="100" width="100" />
+  <div class="mb-10 w-full">
+    <img src="../assets/iftl-logo.png" height="100" width="100" class="m-auto" />
   </div>
 </template>
 <script setup lang="ts"></script>
--- admin-panel/layouts/LayoutDefault.vue
+++ admin-panel/layouts/LayoutDefault.vue
@@ -2,11 +2,106 @@
   <div class="layout">
     <Sidebar>
       <Logo />
-      <Link href="/surveys"> Surveys </Link>
-      <Link href="/archive"> Archive </Link>
-      <Link href="/active-beacons"> Active Beacons </Link>
-      <Link href="/data"> Data </Link>
-      <Link href="/danger-zone"> Danger Zone </Link>
+      <Link href="/surveys" class="flex items-center p-2">
+        <svg
+          class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 5h6m-6 4h6M10 3v4h4V3h-4Z"
+          />
+        </svg>
+        <span>Surveys</span>
+      </Link>
+      <Link href="/archive" class="flex items-center p-2"
+        ><svg
+          class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 12v1h4v-1m4 7H6a1 1 0 0 1-1-1V9h14v9a1 1 0 0 1-1 1ZM4 5h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"
+          />
+        </svg>
+        Archive
+      </Link>
+      <Link href="/active-beacons" class="flex items-center p-2">
+        <svg
+          class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-miterlimit="10"
+            stroke-width="2"
+            d="M7.42584 18.2443c1.3169-2.2023 2.5334-1.5242 3.85026-3.7265 1.3169-2.2023.1004-2.8805 1.4173-5.08281 1.3169-2.20233 2.5335-1.52419 3.8504-3.72652M10.8472 20.1517c1.3169-2.2024 2.5334-1.5242 3.8503-3.7266 1.3169-2.2023.1004-2.8804 1.4173-5.0828 1.3169-2.20228 2.5334-1.52414 3.8503-3.72647l-6.8428-3.81455C11.8054 6.00361 10.5889 5.32547 9.272 7.5278s-.1004 2.8805-1.4173 5.0828c-1.3169 2.2023-2.5334 1.5242-3.85031 3.7265l6.84281 3.8146Z"
+          />
+        </svg>
+        Active Beacons
+      </Link>
+      <Link href="/data" class="flex items-center p-2"
+        ><svg
+          class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 6c0 1.657-3.134 3-7 3S5 7.657 5 6m14 0c0-1.657-3.134-3-7-3S5 4.343 5 6m14 0v6M5 6v6m0 0c0 1.657 3.134 3 7 3s7-1.343 7-3M5 12v6c0 1.657 3.134 3 7 3s7-1.343 7-3v-6"
+          />
+        </svg>
+        Data
+      </Link>
+      <Link href="/danger-zone" class="flex items-center p-2"
+        ><svg
+          class="w-6 h-6 mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        Danger Zone
+      </Link>
     </Sidebar>
     <Content><slot /></Content>
   </div>
