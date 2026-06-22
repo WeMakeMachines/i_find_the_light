37b5f910ed8d765dcce3b60c8b702e23bcc3b980
Franco Speziali <developer@wemakemachines.com>
2026-06-22 03:02:07 +0200

    Remove custom chart toggling in favour of chart.js native toggle feature

--- admin-panel/pages/readings/@id/Readings.vue
+++ admin-panel/pages/readings/@id/Readings.vue
@@ -1,11 +1,6 @@
 <template>
   <div v-if="hasReadings">
     <h2>Lux Chart</h2>
-    <ul class="grid grid-cols-3 gap-2 w-fit">
-      <li v-for="(beacon, index) in beacons" :key="beacon.id" class="border-b border-dotted">
-        <label>Beacon ID: {{ beacon.id }} <input type="checkbox" v-model="beacons[index].checked" /></label>
-      </li>
-    </ul>
     <canvas id="lux-chart" style="width: 100%; height: 400px"></canvas>
   </div>
   <div v-else><p>No readings collected</p></div>
@@ -14,16 +9,13 @@
 <script lang="ts" setup>
 import "chartjs-adapter-date-fns";
 import { Chart, registerables } from "chart.js";
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref } from "vue";
 
 import type { ReadingsByBeaconId } from "../../../../types/sqlite";
 
 const props = defineProps<{ initialReadings: ReadingsByBeaconId }>();
 const readingsByBeaconId = ref(props.initialReadings);
 const hasReadings = readingsByBeaconId === null ? false : true;
-const beacons = ref(mapBeacons(readingsByBeaconId.value));
-
-let chart: Chart | null = null;
 
 onMounted(async () => {
   if (hasReadings) {
@@ -31,7 +23,7 @@ onMounted(async () => {
 
     const ctx = document.getElementById("lux-chart");
 
-    chart = new Chart(ctx, {
+    new Chart(ctx, {
       type: "line",
       data: {
         datasets: mapBeaconReadingsToLineChart(readingsByBeaconId.value),
@@ -47,36 +39,14 @@ onMounted(async () => {
   }
 });
 
-watch(
-  beacons,
-  () => {
-    if (chart === null) return;
-
-    chart.data.datasets = mapBeaconReadingsToLineChart(readingsByBeaconId.value);
-    chart.update("none");
-  },
-  { deep: true },
-);
-
-function mapBeacons(readings: ReadingsByBeaconId | null): { id: string; checked: boolean }[] {
-  if (!readings) return [];
-
-  return Object.keys(readings).map((beaconId) => ({ id: beaconId, checked: true }));
-}
-
 function mapBeaconReadingsToLineChart(
   readings: ReadingsByBeaconId,
 ): { label: string; data: { x: number; y: number }[] }[] {
   const mappedReadings: { label: string; data: { x: number; y: number }[] }[] = [];
 
   for (const [beaconId, beaconData] of Object.entries(readings)) {
-    // check if beaconId has been unchecked
-    const checked = Boolean(beacons.value.find((beacon) => beacon.id === beaconId && beacon.checked));
-
-    if (!checked) continue;
-
     const dataset = {
-      label: `ID ${beaconId}`,
+      label: `Beacon ID ${beaconId}`,
       data: beaconData.map((data) => ({ x: data.readingTimestamp, y: data.lux })),
     };
 
