5d1433fa064acde44404cb0c65736d2340a3bd0e
Franco Speziali <developer@wemakemachines.com>
2026-06-24 11:13:21 +0200

    Re-organise bash scripts

--- install.sh
+++ install.sh
@@ -14,44 +14,4 @@ git clone https://github.com/WeMakeMachines/i_find_the_light.git .
 cp .env.sample .env
 bun install
 
-# Install service
-APP_NAME="iftl"
-SERVICE_FILE="/etc/systemd/system/$APP_NAME.service"
-APP_EXECUTABLE="/opt/iftl/$APP_NAME.sh"
-
-# Give permissions to executable
-chmod +x "$APP_EXECUTABLE"
-
-# Create systemd service file
-cat <<EOF > "$SERVICE_FILE"
-[Unit]
-Description=I find the light (station)
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/bin/bash $APP_EXECUTABLE
-Restart=always
-RestartSec=1
-User=${USER}
-WorkingDirectory=/opt/iftl
-StandardOutput=journal
-StandardError=journal
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Set correct permissions
-chmod 644 "$SERVICE_FILE"
-
-# Reload systemd and enable service
-systemctl daemon-reload
-systemctl enable "$APP_NAME"
-
-echo "Installation complete. Running service with default parameters..."
-
-systemctl start "$APP_NAME"
-systemctl status "$APP_NAME"
-
-echo "Install complete! 🌿"
+./create-systemd-service.sh
